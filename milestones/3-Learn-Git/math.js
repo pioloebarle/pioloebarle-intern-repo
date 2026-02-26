e93a48b49478f90622fb94c939f2a081f5e0d1fc
pioloebarle <besingapiolo@gmail.com>
2026-02-26 13:38:12 +0800

    update git_understanding.md with git bisect hands-on scenario

--- milestones/3-Learn-Git/math.js
+++ milestones/3-Learn-Git/math.js
@@ -3,16 +3,11 @@ function add(a, b) {
 }
 
 function subtract(a, b) {
-  return a + b;
+  return a - b;
 }
 
 function multiply(a, b) {
   return a * b;
 }
 
-function divide(a, b) {
-    return a / b;
-}
-
 console.log("Math ready");
-
